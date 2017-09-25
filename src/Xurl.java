82339b1fbf7ecd4fea69220368c9f3b5e568a782
Oliver Yates <oliver.yates01@gmail.com>
2017-09-25 12:16:26 +0200

    Added Xurl constructor with proxy+proxy_port support

--- src/Xurl.java
+++ src/Xurl.java
@@ -9,7 +9,6 @@ import java.io.BufferedReader;
 public class Xurl {
 	
 	public Xurl (String url) {
-		System.out.println("test");
 		Socket _socket_ = null;
 		try {
 			/*Verify URL*/
@@ -40,7 +39,6 @@ public class Xurl {
 				int code = Integer.parseInt(parsed_line[1]);
 				if (code == 200) {
 					System.out.println(parsed_line[2]);
-					System.out.println("Test 2");
 				}
 				else if (code == 301){
 					System.out.println(parsed_line[2]);
@@ -75,5 +73,72 @@ public class Xurl {
 			/*Exception from MyURL*/
 			System.out.println(e.getMessage());
 		}		
+	}
+	
+	public Xurl(String url, String proxy, int proxy_port) {
+		Socket _socket_ = null;
+		try {
+			/*Verify URL*/
+			MyURL _url_ = new MyURL(url);
+			
+			/*Create a socket*/
+			
+			int port = _url_.port; /// Case when port wasn't specify
+			if (port == -1 ) {
+				port = 80; 
+			}
+			_socket_ = new Socket(proxy, proxy_port); ///What happens when port == -1 ?
+			
+			/* Output */
+			OutputStream out = _socket_.getOutputStream();
+			PrintStream output = new PrintStream(out);
+			output.print("GET"+ url + " HTTP/1.0\r\n");
+
+			/*Input*/
+			InputStream in = _socket_.getInputStream();
+			InputStreamReader in_reader = new InputStreamReader(in);
+			BufferedReader bufferedreader = new BufferedReader(in_reader);
+			String line = new String();
+			while ((line = bufferedreader.readLine()) != null) {
+				String[] parsed_line = line.split(" ", 3);
+				System.out.println(parsed_line);
+				/*Code determination*/
+				int code = Integer.parseInt(parsed_line[1]);
+				if (code == 200) {
+					System.out.println(parsed_line[2]);
+				}
+				else if (code == 301){
+					System.out.println(parsed_line[2]);
+					System.out.println("You will be redirected to your page");
+				}
+				else if (code == 302){
+					System.out.println(parsed_line[2]);
+					System.out.println("You will be redirected to your page");
+				}
+				else if (code == 400){
+					System.out.println(parsed_line[2]);
+					System.out.println("Please retry with a correct address");
+				}
+				else if (code == 404){
+					System.out.println(parsed_line[2]);
+					System.out.println("Page not found, please retry");
+				}
+			}		
+		}
+		catch(IOException e) {
+			/*Exception from the Socket creation*/
+			if (_socket_ != null) {
+				try {
+					_socket_.close();
+				}
+				catch(IOException ioclose) {
+					System.out.println(ioclose.getMessage());
+				}
+			}
+		}
+		catch(IllegalArgumentException e) {
+			/*Exception from MyURL*/
+			System.out.println(e.getMessage());
+		}
 	}
 }
